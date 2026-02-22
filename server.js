518d16399873b2917e40fefab8c05477d8394187
domxenh <manhghep1908951@gmail.com>
2026-02-22 15:30:07 +0700

    fix routing structure

--- server.js
+++ server.js
@@ -1,35 +1,16 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-const axios = require("axios");
 
-// Cho phép đọc dữ liệu JSON từ form
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Cho phép dùng file tĩnh (HTML, CSS)
 app.use(express.static("public"));
 
-// Route test
+// Route trang chủ
 app.get("/", (req, res) => {
-    res.send("Server đang chạy 🚀");
+    res.sendFile(path.join(__dirname, "public/trang-chu/index.html"));
 });
 
 app.listen(PORT, () => {
     console.log(`Server chạy tại http://localhost:${PORT}`);
-});
-app.post("/order", async (req, res) => {
-    const { name, product } = req.body;
-
-    try {
-        await axios.post("https://script.google.com/macros/s/AKfycbwB-PTMyXpgJQezZtRd-0DJojnP6O6Vkq_YYLFRj45Nix41Jz464OzlHTPZLm3iSQgH/exec", {
-            name: name,
-            product: product
-        });
-
-        res.send("Đặt hàng thành công và đã lưu vào Google Sheets!");
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Lỗi gửi dữ liệu lên Google");
-    }
 });

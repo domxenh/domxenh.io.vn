88cf5ef09120614c444f7a639ea7f9f0fb561967
domxenh <manhghep1908951@gmail.com>
2026-02-22 15:36:05 +0700

    update homepage final

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+// Cho phép dùng file tĩnh
+app.use(express.static(path.join(__dirname, "public")));
 
-// Route trang chủ
+// Trang chủ
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/trang-chu/index.html"));
+  res.sendFile(path.join(__dirname, "public", "trang-chu", "index.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`Server chạy tại http://localhost:${PORT}`);
+  console.log(`Server chạy tại http://localhost:${PORT}`);
 });

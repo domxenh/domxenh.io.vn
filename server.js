fd7b9ff8099eeb009c7a9dc34c1a0d1dddb5d1a8
domxenh <manhghep1908951@gmail.com>
2026-02-22 15:46:35 +0700

    update giao dien test

--- server.js
+++ server.js
@@ -4,12 +4,22 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Cho phép dùng file tĩnh
+// Cho phép load toàn bộ file trong public
 app.use(express.static(path.join(__dirname, "public")));
 
-// Trang chủ
+// Route trang chủ
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "trang-chu", "index.html"));
+});
+
+// Route sản phẩm
+app.get("/san-pham", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "san-pham", "index.html"));
+});
+
+// Route liên hệ
+app.get("/lien-he", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "lien-he", "index.html"));
 });
 
 app.listen(PORT, () => {

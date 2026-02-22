75a704f6958af3f543c607b26804f45b016662d3
domxenh <manhghep1908951@gmail.com>
2026-02-22 15:14:26 +0700

    fix port for render

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const axios = require("axios");
 
 // Cho phép đọc dữ liệu JSON từ form

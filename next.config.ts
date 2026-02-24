5933e0f225d32cb5c406ea771e84b3206eebffab
domxenh <manhghep1908951@gmail.com>
2026-02-24 18:02:45 +0700

    Update V1.1

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,5 @@
-// Code 1
-
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.domxenh.io.vn",
-          },
-        ],
-        destination: "https://domxenh.io.vn/:path*",
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
-
-// End code 1
